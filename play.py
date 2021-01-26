41f2e315ee8478b19400d5491355036a42f6b9f6
EngEmmanuel <emmanueloladokun@hotmail.co.uk>
2021-01-26 16:45:17 +0000

    Updates to the environment and pipeline

--- play.py
+++ play.py
@@ -1,2 +1,6 @@
 # Develop a model for your target only
-# Then try and create a model on all targets
+# Then try and create a model on all targets 
+
+from sklearn import preprocessing
+
+j = preprocessing.StandardScaler()
